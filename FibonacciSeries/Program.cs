c9d5c30b692d9ce08c0a27ab36d6ab1aa4f61ed7
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-09-15 22:43:55 +0530

    [Pratu]ADDED: Fibonacci Series Up To GIven Number

---
+++ FibonacciSeries/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Initilazing the variables
+            int firstNumber = 0, SecondNumber = 1, nextNumber, number;
+            Console.Write("Enter the number upto which print the Fibonacci series : ");
+            number = int.Parse(Console.ReadLine());
+
+            //First print first and second number
+            Console.Write(firstNumber + " " + SecondNumber + " ");
+            nextNumber = firstNumber + SecondNumber;
+
+            //Starts the loop from 2 because 0 and 1 are already printed
+            for (int i = 2; nextNumber < number; i++)
+            {
+                Console.Write(nextNumber + " ");
+                firstNumber = SecondNumber;
+                SecondNumber = nextNumber;
+                nextNumber = firstNumber + SecondNumber;
+            }
+        }
+    }
+}
