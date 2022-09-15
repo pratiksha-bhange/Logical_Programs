d7844a030ec6571e631167f2f8d5533348caa8d0
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-09-15 23:03:47 +0530

    [Pratu]ADDED: Method To Find a Perfect Number

---
+++ PerfectNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number, sum = 0, n;
+            Console.Write("Enter the Number : ");
+            number = int.Parse(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("Entered number is a perfect number");
+
+            }
+            else
+            {
+                Console.WriteLine("Entered number is not a perfect number");
+            }
+        }
+    }
+}
