c7377481763232329d8ce28847baf39d45586e05
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-09-15 23:18:37 +0530

    [Pratu]ADDED: Method To A Find Prime Numbers

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number : ");
+            int number = int.Parse(Console.ReadLine());
+            bool IsPrime = true;
+            for (int i = 2; i < number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    IsPrime = false;
+                    break;
+                }
+            }
+            if (IsPrime)
+            {
+                Console.Write("Number is Prime.");
+            }
+            else
+            {
+                Console.Write("Number is not Prime.");
+            }
+        }
+    }
+}
