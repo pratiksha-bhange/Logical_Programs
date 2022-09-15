adab573cf3a8a8b975df6b893bc3f8119cd73d1e
pratiksha-bhange <108232994+pratiksha-bhange@users.noreply.github.com>
2022-09-15 23:48:01 +0530

    [Pratu]ADDED: Simulate Stopwatch Program

---
+++ Stopwatch Program/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+namespace Stopwatch_Program
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            // Create new stopwatch.
+            Stopwatch stopwatch = new Stopwatch();
+
+            Console.WriteLine("Press 1 to start stopwatch");
+            string start = Console.ReadLine().ToUpper();
+            if (start == "1")
+            {
+                // Begin timing.
+                stopwatch.Start();
+
+                Console.WriteLine("Press 2 to start stopwatch");
+                string stop = Console.ReadLine().ToUpper();
+                if (stop == "2")
+                {
+                    // Stop timing.
+                    stopwatch.Stop();
+                }
+            }
+            // Write hours, minutes and seconds.
+            Console.WriteLine("Time elapsed: {0:hh\\:mm\\:ss}", stopwatch.Elapsed);
+
+        }
+    }
+}
